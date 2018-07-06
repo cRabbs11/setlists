cbf35da11c5c7bfb8453a58bc7cb06918195c6ea
DESKTOP-LAI6C0V\Evgeny <eskocvhkov@primesoftpro.ru>
2018-07-06 09:42:13 +0300

    добавил класс для артиста

---
+++ app/src/main/java/com/example/evgeny/setlist_mobile/Artist.java
@@ -0,0 +1,11 @@
+package com.example.evgeny.setlist_mobile;
+
+/**
+ * Created by Evgeny on 06.07.2018.
+ */
+
+public class Artist {
+    public String name;
+    public String sortName;
+    public String url;
+}
