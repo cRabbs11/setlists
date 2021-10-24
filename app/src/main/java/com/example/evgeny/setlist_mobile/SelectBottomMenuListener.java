e14595f22199637fc6375fe48e8f257151776081
evgenykochkov <crabbs100@gmail.com>
2021-10-24 19:28:59 +0300

    перевел SelectBottomMenuListener в kotlin

--- app/src/main/java/com/example/evgeny/setlist_mobile/SelectBottomMenuListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.evgeny.setlist_mobile;
-
-import com.example.evgeny.setlist_mobile.model.BaseModel;
-
-public interface SelectBottomMenuListener {
-    public <T extends BaseModel> void setMenuItem  (String item, T object);
-}
