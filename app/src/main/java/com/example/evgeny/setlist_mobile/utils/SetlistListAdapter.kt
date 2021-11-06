ab4005f69b481ba0e48e5f77cca3cc7053b80595
evgenykochkov <crabbs100@gmail.com>
2021-11-06 20:01:07 +0300

    сделал методы добавления item в SetlistListAdapter.kt, убрал из них notifyDataSetChanged

--- app/src/main/java/com/example/evgeny/setlist_mobile/utils/SetlistListAdapter.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/utils/SetlistListAdapter.kt
@@ -40,15 +40,27 @@ class SetlistListAdapter(clickListener: OnItemClickListener<Setlist>) : Recycler
     }
 
     fun setItems(list: List<Setlist>) {
+        clearItems()
         list.forEach {
             setlists.add(it)
         }
-        notifyDataSetChanged()
+    }
+
+    fun addItem(setlist: Setlist) {
+        setlists.add(setlist)
+    }
+
+    fun addUniqItems(list: List<Setlist>) {
+        list.forEach {
+            if(!setlists.contains(it)) {
+                setlists.add(it)
+                Log.d(TAG, "add: ${it.eventDate}")
+            }
+        }
     }
 
     fun clearItems() {
         setlists.clear()
-        notifyDataSetChanged()
     }
 
     override fun onBindViewHolder(holder: SetlistHolder, position: Int) {
