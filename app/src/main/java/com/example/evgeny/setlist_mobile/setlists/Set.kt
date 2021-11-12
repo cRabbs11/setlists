4978f430c7e23723917c873c14c7c9986e6dea3b
evgenykochkov <crabbs100@gmail.com>
2021-11-12 11:46:12 +0300

    добавил имплементацию SongListItem, переопределил toString

--- app/src/main/java/com/example/evgeny/setlist_mobile/setlists/Set.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/setlists/Set.kt
@@ -1,5 +1,8 @@
 package com.example.evgeny.setlist_mobile.setlists
 
-data class Set(val name: String, val number: String, val encore: String, val songs: List<Song> = arrayListOf()) : BaseModel() {
+data class Set(val name: String, val number: String, val encore: String, val songs: List<Song> = arrayListOf()) : BaseModel(), SongListItem {
 
+    override fun toString(): String {
+        return "Set(name='$name', number='$number', encore='$encore', songs=$songs)"
+    }
 }
--- app/src/main/java/com/example/evgeny/setlist_mobile/setlists/Song.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/setlists/Song.kt
@@ -2,4 +2,7 @@ package com.example.evgeny.setlist_mobile.setlists
 
 data class Song(val name: String, val info: String, val tape: Boolean, val cover: Artist?, val with: Artist?, var number: Int) : BaseModel(), SongListItem {
 
+    override fun toString(): String {
+        return "Song(name='$name', info='$info', tape=$tape, cover=$cover, with=$with, number=$number)"
+    }
 }
