9945d000f76d3c5de78aeb356c74ab58c562398d
evgenykochkov <crabbs100@gmail.com>
2021-12-17 23:25:48 +0300

    перенес функции BottomNavigationActivity.kt в MainActivity.kt

--- app/src/main/java/com/example/evgeny/setlist_mobile/MainActivity.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/MainActivity.kt
@@ -1,15 +1,35 @@
 package com.example.evgeny.setlist_mobile
 
 import android.os.Bundle
+import android.text.util.Linkify
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.evgeny.setlist_mobile.artistSearch.ArtistSearchFragment
+import java.util.regex.Pattern
 
-class MainActivity: BottomNavigationActivity() {
+class MainActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        openLaunchFragment()
+        setContentView(R.layout.activity_main)
+        val toSource = findViewById<TextView>(R.id.toSource)
+        val pattern = Pattern.compile("www.setlist.fm")
+        Linkify.addLinks(toSource ,pattern,"http://")
+
+        openArtistSearchFragment()
     }
 
     fun setActionBarTitle(title: String?) {
         supportActionBar!!.title = title
+    }
+
+    fun openArtistSearchFragment() {
+
+        val fragment = ArtistSearchFragment()
+
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .addToBackStack(null)
+                .commit()
     }
 }
