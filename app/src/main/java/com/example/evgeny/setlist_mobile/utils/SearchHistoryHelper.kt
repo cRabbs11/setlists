114e41c54949d89f4bb0de2a08e1ffb44665fb9a
evgenykochkov <crabbs100@gmail.com>
2021-12-19 18:50:25 +0300

    сделал класс для сохранения, чтения истории поиска

---
+++ app/src/main/java/com/example/evgeny/setlist_mobile/utils/SearchHistoryHelper.kt
@@ -0,0 +1,28 @@
+package com.example.evgeny.setlist_mobile.utils
+
+import android.content.Context
+import java.io.File
+
+class SearchHistoryHelper(val context: Context) {
+
+    private val searchHistoryFilename = "search_history"
+
+    private fun appendInFile(filename: String, text: String) {
+        val file = File(context.filesDir, filename)
+        file.appendText(text)
+        file.appendText("\n")
+    }
+
+    private fun readLinesFromFile(filename: String): List<String> {
+        val file = File(context.filesDir, filename)
+        return  file.readLines()
+    }
+
+    fun saveSearchQuery(text: String) {
+        appendInFile(searchHistoryFilename, text)
+    }
+
+    fun getHistorySearchList(): List<String> {
+        return readLinesFromFile(searchHistoryFilename)
+    }
+}
