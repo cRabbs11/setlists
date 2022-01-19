fcc4f6fcb1775bf8dc34997da1b61cc2ba75fdf9
evgenykochkov <crabbs100@gmail.com>
2022-01-19 21:18:10 +0300

    рефакторинг

--- app/src/main/java/com/example/evgeny/setlist_mobile/artistSearch/ArtistSearchFragment.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/artistSearch/ArtistSearchFragment.kt
@@ -63,11 +63,11 @@ class ArtistSearchFragment : Fragment(), ArtistSearchContract.View, OnItemClickL
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentSearchConstraintBinding.inflate(inflater, container, false)
         val rootView = binding.root
-        initView(rootView)
+        initView()
         return rootView
     }
 
-    fun initView(rootView: View ) {
+    fun initView() {
         Log.d(TAG, " запустили")
         binding.recyclerView.itemAnimator = ItemListAnimator(requireContext())
         //val linearLayoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
@@ -131,9 +131,9 @@ class ArtistSearchFragment : Fragment(), ArtistSearchContract.View, OnItemClickL
         (activity as MainActivity).openSetlistsSearchFragment()
     }
 
-    override fun onItemClick(t: Artist) {
+    override fun onItemClick(artist: Artist) {
         Log.d(TAG, "onItemClick")
-        presenter.onListItemClicked(t)
+        presenter.onListItemClicked(artist)
     }
 
     private fun populateAdapter(query: String) {
