a046bc35ef3642247ba0cd5a8009fe0817f2b53b
evgenykochkov <crabbs100@gmail.com>
2022-02-09 19:59:06 +0300

    поправил поля класса Artist

--- app/src/main/java/com/example/evgeny/setlist_mobile/setlists/Artitst.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/setlists/Artitst.kt
@@ -1,5 +1,10 @@
 package com.example.evgeny.setlist_mobile.setlists
 
-data class Artist(val mbid: String, val  name: String, val sortName: String, val url: String) : BaseModel() {
-   lateinit var disambiguation : String
-}
+data class Artist(
+        val disambiguation: String,
+        val mbid: String,
+        val name: String,
+        val sortName: String,
+        val tmid: Int,
+        val url: String
+) : BaseModel()
