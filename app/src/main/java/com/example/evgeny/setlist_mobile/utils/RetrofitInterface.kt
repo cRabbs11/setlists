92d58478db0767e258ad2fdbc3aa2d02a444c0a7
evgenykochkov <crabbs100@gmail.com>
2022-02-09 20:02:53 +0300

    сделал интерфейс для запросов через Retrofit

---
+++ app/src/main/java/com/example/evgeny/setlist_mobile/utils/RetrofitInterface.kt
@@ -0,0 +1,18 @@
+package com.example.evgeny.setlist_mobile.utils
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface RetrofitInterface {
+
+    @GET("search/artists")
+    fun searchArtists(
+            @Header("x-api-key") apiKey: String = SetlistAPI.KEY,
+            @Header("Accept") accept: String = "application/json",
+            @Query("artistName") artistName: String,
+            @Query("page") page: Int,
+            @Query("sort") sort: String
+    ): Call<ArtistData>
+}
