4453a69afe9730104a6c50775748050fd8e97dbb
evgenykochkov <crabbs100@gmail.com>
2022-02-09 20:16:55 +0300

    переделал поиск артиста в презентере на retorift

--- app/src/main/java/com/example/evgeny/setlist_mobile/artistSearch/ArtistSearchPresenter.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/artistSearch/ArtistSearchPresenter.kt
@@ -1,7 +1,5 @@
 package com.example.evgeny.setlist_mobile.artistSearch
 
-import android.content.ContentValues.TAG
-import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
@@ -9,15 +7,13 @@ import android.util.Log
 
 import com.example.evgeny.setlist_mobile.mvp.PresenterBase
 import com.example.evgeny.setlist_mobile.setlists.Artist
-import com.example.evgeny.setlist_mobile.setlists.ParserKotlin
 import com.example.evgeny.setlist_mobile.setlists.Setlist
-import com.example.evgeny.setlist_mobile.setlists.SetlistsAPI
 import com.example.evgeny.setlist_mobile.utils.AnswerListener
+import com.example.evgeny.setlist_mobile.utils.RetrofitSetlists
 import com.example.evgeny.setlist_mobile.utils.SearchHistoryHelper
 import com.example.evgeny.setlist_mobile.utils.SetlistsRepository
 
-
-public class ArtistSearchPresenter(setlistsRepository: SetlistsRepository, val searchHistoryHelper: SearchHistoryHelper):
+class ArtistSearchPresenter(setlistsRepository: SetlistsRepository, val searchHistoryHelper: SearchHistoryHelper):
         PresenterBase<ArtistSearchContract.View>(), ArtistSearchContract.Presenter {
 
 	private fun showAddWordDialog() {
@@ -43,11 +39,17 @@ public class ArtistSearchPresenter(setlistsRepository: SetlistsRepository, val s
 
 	override fun onSearchArtistClicked(artistName: String) {
 		if (artistName != "") {
-			searchArtists(artistName, object : AnswerListener<List<Artist>> {
+			val retrofitSetlists = RetrofitSetlists()
+			retrofitSetlists.searchArtists(artistName, object: AnswerListener<List<Artist>> {
 				override fun getAnswer(t: List<Artist>) {
-					Log.d(TAG, "пришли")
-					getView()?.showArtistList(t)
-					if (t.isEmpty()) {
+					if (t.isNotEmpty()) {
+						var isInHistory = false
+						searchHistoryHelper.getHistorySearchList().forEach {
+							if (it == artistName) { isInHistory = true }
+						}
+						if (!isInHistory) { searchHistoryHelper.saveSearchQuery(artistName) }
+						getView()?.showArtistList(t)
+					} else {
 						getView()?.showToast("артистов не найдено")
 					}
 				}
@@ -68,35 +70,6 @@ public class ArtistSearchPresenter(setlistsRepository: SetlistsRepository, val s
     init {
 		this.setlistsRepository = setlistsRepository
     }
-
-	private fun searchArtists(artistName: String, answerListener: AnswerListener<List<Artist>>) {
-		val artists = ArrayList<Artist>()
-		val handler = object : Handler(Looper.getMainLooper()) {
-			override fun handleMessage(msg: Message) {
-				answerListener.getAnswer(artists)
-			}
-		}
-
-		val runnable = Runnable {
-			        kotlin.run {
-						setlistsRepository.newSearchArtists(artistName).forEach {
-							artists.add(it)
-						}
-
-						if (artists.isNotEmpty()) {
-							var isInHistory = false
-							searchHistoryHelper.getHistorySearchList().forEach {
-								if (it == artistName) { isInHistory = true }
-							}
-							if (!isInHistory) { searchHistoryHelper.saveSearchQuery(artistName) }
-						}
-						handler.sendEmptyMessage(1)
-			}
-		}
-
-		val thread = Thread(runnable)
-		thread.start()
-	}
 
 	private fun searchSetlists(artistMbid: String, answerListener: AnswerListener<List<Setlist>>) {
 		val handler = object : Handler(Looper.getMainLooper()) {
