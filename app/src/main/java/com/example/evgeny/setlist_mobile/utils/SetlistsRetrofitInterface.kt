6273c703f1fe13f3605ce41e1ed832aa4453b57b
evgenykochkov <crabbs100@gmail.com>
2022-02-13 17:23:00 +0300

    поправил имя интерфейса для работы Retrofit с SetlistsAPI

--- app/src/main/java/com/example/evgeny/setlist_mobile/utils/SetlistsRetrofitInterface.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/utils/SetlistsRetrofitInterface.kt
@@ -1,16 +1,17 @@
 package com.example.evgeny.setlist_mobile.utils
 
+import com.example.evgeny.setlist_mobile.data.entity.ArtistDataDTO
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
 
-interface SetlistsAPIInterface {
+interface SetlistsRetrofitInterface {
 
     @GET("search/artists")
     fun searchArtists(
-            @Header("x-api-key") apiKey: String = SetlistAPI.KEY,
-            @Header("Accept") accept: String = "application/json",
+            @Header("x-api-key") apiKey: String,
+            @Header("Accept") accept: String,
             @Query("artistName") artistName: String,
             @Query("page") page: Int,
             @Query("sort") sort: String
