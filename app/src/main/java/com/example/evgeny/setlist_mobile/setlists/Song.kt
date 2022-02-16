6fd693b421c5a4d5d14d508011e3c708a80c373d
evgenykochkov <crabbs100@gmail.com>
2022-02-16 21:23:04 +0300

    сделал nullable поля в классах Set и Song, убрал лишнее поле number в Set

--- app/src/main/java/com/example/evgeny/setlist_mobile/setlists/Song.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/setlists/Song.kt
@@ -2,7 +2,7 @@ package com.example.evgeny.setlist_mobile.setlists
 
 import com.example.evgeny.setlist_mobile.data.Artist
 
-data class Song(val name: String, val info: String, val tape: Boolean, val cover: Artist?, val with: Artist?, var number: Int) : BaseModel(), SongListItem {
+data class Song(val name: String, val info: String?, val tape: Boolean, val cover: Artist?, val with: Artist?, var number: Int) : BaseModel(), SongListItem {
 
     override fun toString(): String {
         return "Song(name='$name', info='$info', tape=$tape, cover=$cover, with=$with, number=$number)"
