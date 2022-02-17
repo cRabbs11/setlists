a996cb3859185e951fb417e46a570782f2680e37
evgenykochkov <crabbs100@gmail.com>
2022-02-17 20:02:11 +0300

    добавил в RetrofitInterface GET запрос на список сетлистов артиста

--- app/src/main/java/com/example/evgeny/setlist_mobile/utils/SetlistsRetrofitInterface.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/utils/SetlistsRetrofitInterface.kt
@@ -1,9 +1,11 @@
 package com.example.evgeny.setlist_mobile.utils
 
 import com.example.evgeny.setlist_mobile.data.entity.ArtistDataDTO
+import com.example.evgeny.setlist_mobile.data.entity.SetlistsDataDTO
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface SetlistsRetrofitInterface {
@@ -16,4 +18,13 @@ interface SetlistsRetrofitInterface {
             @Query("page") page: Int,
             @Query("sort") sort: String
     ): Call<ArtistDataDTO>
+
+    @GET("artist/{artistMbid}/setlists")
+    fun getArtistSetlists(
+            @Path("artistMbid") artistMbid: String,
+            @Header("x-api-key") apiKey: String,
+            @Header("Accept") accept: String,
+            @Query("p") page: Int
+
+    ): Call<SetlistsDataDTO>
 }
