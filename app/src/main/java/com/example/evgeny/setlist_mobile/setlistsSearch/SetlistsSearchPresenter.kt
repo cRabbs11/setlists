ea81b47f4fb78d8182ebe89d985223ad9f6a6b29
evgenykochkov <crabbs100@gmail.com>
2022-02-17 20:03:44 +0300

    в SetlistsSearchFragment.kt переделал получения списка сетлистов на Retrofit

--- app/src/main/java/com/example/evgeny/setlist_mobile/setlistsSearch/SetlistsSearchPresenter.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/setlistsSearch/SetlistsSearchPresenter.kt
@@ -1,17 +1,15 @@
 package com.example.evgeny.setlist_mobile.setlistsSearch
 
-
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
-import android.util.Log
+import com.example.evgeny.setlist_mobile.data.entity.SetlistsDataDTO
 import com.example.evgeny.setlist_mobile.mvp.PresenterBase
 import com.example.evgeny.setlist_mobile.setlists.Setlist
-import com.example.evgeny.setlist_mobile.utils.AnswerListener
-import com.example.evgeny.setlist_mobile.utils.SetlistsRepository
+import com.example.evgeny.setlist_mobile.utils.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
-public class SetlistsSearchPresenter(setlistsRepository: SetlistsRepository):
+public class SetlistsSearchPresenter(setlistsRepository: SetlistsRepository, val setlistsRetrofit: SetlistsRetrofitInterface):
         PresenterBase<SetlistsSearchContract.View>(), SetlistsSearchContract.Presenter {
 
 	private val TAG = SetlistsSearchPresenter::class.java.name + " BMTH "
@@ -27,10 +25,30 @@ public class SetlistsSearchPresenter(setlistsRepository: SetlistsRepository):
 		//Log.d(TAG, "onRecyclerViewScrolled")
 		//Log.d(TAG, "lastVisiblePos=$lastVisiblePos, size= ${setlistsRepository.getSetlists().size}")
 		if (!isLoading && lastVisiblePos>=totalPosCount-1) {
-  			addMoreSetlists(setlistsRepository.getCurrentArtist()!!.mbid, object: AnswerListener<List<Setlist>> {
-				override fun getAnswer(t: List<Setlist>) {
-					getView()?.showSetlistList(t)
+			isLoading=true
+			setlistsRetrofit.getArtistSetlists(
+					artistMbid = setlistsRepository.getCurrentArtist()!!.mbid,
+					apiKey = ApiKeys.SETLISTS_API_KEY,
+					accept = SetlistsAPIConstants.ACCEPT_HEADER,
+					page = setlistsRepository.getSetlistPage()
+			).enqueue(object: Callback<SetlistsDataDTO> {
+				override fun onResponse(call: Call<SetlistsDataDTO>, response: Response<SetlistsDataDTO>) {
+					val list = Converter.convertSetlistDTOListToSetlistList(response.body()?.setlist)
+					if (list.isNotEmpty()) {
+						setlistsRepository.addToSetlists(list)
+						getView()?.showSetlistList(setlistsRepository.getSetlists())
+						setlistsRepository.increaseSetlistPage()
+					} else {
+						getView()?.showToast("сетлистов для ${setlistsRepository.getCurrentArtist()?.name} не найдено")
+					}
+					isLoading=false
 				}
+
+				override fun onFailure(call: Call<SetlistsDataDTO>, t: Throwable) {
+					getView()?.showToast("поиск не удался")
+					isLoading=false
+				}
+
 			})
 		}
 	}
@@ -43,13 +61,27 @@ public class SetlistsSearchPresenter(setlistsRepository: SetlistsRepository):
 		} else {
 			val artist = setlistsRepository.getCurrentArtist()
 			if (artist!=null) {
-				searchSetlists(artist.mbid, object : AnswerListener<List<Setlist>> {
-					override fun getAnswer(t: List<Setlist>) {
-						getView()?.showSetlistList(t)
-						if (t.isEmpty()) {
+				setlistsRetrofit.getArtistSetlists(
+						artistMbid = artist.mbid,
+						apiKey = ApiKeys.SETLISTS_API_KEY,
+						accept = SetlistsAPIConstants.ACCEPT_HEADER,
+						page = setlistsRepository.getSetlistPage()
+				).enqueue(object: Callback<SetlistsDataDTO> {
+					override fun onResponse(call: Call<SetlistsDataDTO>, response: Response<SetlistsDataDTO>) {
+						val list = Converter.convertSetlistDTOListToSetlistList(response.body()?.setlist)
+						if (list.isNotEmpty()) {
+							setlistsRepository.setNewSetlists(list)
+							getView()?.showSetlistList(list)
+						} else {
 							getView()?.showToast("сетлистов для ${artist.name} не найдено")
 						}
+
 					}
+
+					override fun onFailure(call: Call<SetlistsDataDTO>, t: Throwable) {
+						getView()?.showToast("поиск не удался")
+					}
+
 				})
 			}
 		}
@@ -58,53 +90,4 @@ public class SetlistsSearchPresenter(setlistsRepository: SetlistsRepository):
     init {
 		this.setlistsRepository = setlistsRepository
     }
-
-	private fun searchSetlists(artistMbid: String, answerListener: AnswerListener<List<Setlist>>) {
-		val handler = object : Handler(Looper.getMainLooper()) {
-			override fun handleMessage(msg: Message) {
-				answerListener.getAnswer(setlistsRepository.getSetlists())
-			}
-		}
-
-		val runnable = Runnable {
-			val setlists = ArrayList<Setlist>()
-			kotlin.run {
-				setlistsRepository.searchSetlists(artistMbid).forEach {
-					setlists.add(it)
-				}
-				setlistsRepository.setNewSetlists(setlists)
-				handler.sendEmptyMessage(1)
-			}
-		}
-
-		val thread = Thread(runnable)
-		thread.start()
-	}
-
-	private fun addMoreSetlists(artistMbid: String, answerListener: AnswerListener<List<Setlist>>) {
-		isLoading=true
-		val handler = object : Handler(Looper.getMainLooper()) {
-			override fun handleMessage(msg: Message) {
-				answerListener.getAnswer(setlistsRepository.getSetlists())
-				setlistsRepository.increaseSetlistPage()
-				isLoading=false
-			}
-		}
-
-		val runnable = Runnable {
-			val setlists = ArrayList<Setlist>()
-			kotlin.run {
-				setlistsRepository.searchSetlists(artistMbid, setlistsRepository.getSetlistPage()).forEach {
-					setlists.add(it)
-				}
-				setlistsRepository.addToSetlists(setlists)
-				handler.sendEmptyMessage(1)
-			}
-		}
-
-		val thread = Thread(runnable)
-		thread.start()
-	}
-
-
 }
