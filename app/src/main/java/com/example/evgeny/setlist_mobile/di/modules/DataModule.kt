f89b6ea6caf6e2f2e68eb6cd0cc3301c33d0bb6a
evgenykochkov <crabbs100@gmail.com>
2022-03-21 20:00:09 +0300

    добавил БД с Artist в dagger

--- app/src/main/java/com/example/evgeny/setlist_mobile/di/modules/DataModule.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/di/modules/DataModule.kt
@@ -1,6 +1,9 @@
 package com.example.evgeny.setlist_mobile.di.modules
 
 import android.content.Context
+import androidx.room.Room
+import com.example.evgeny.setlist_mobile.data.AppDataBase
+import com.example.evgeny.setlist_mobile.data.dao.ArtistDao
 import com.example.evgeny.setlist_mobile.utils.SearchHistoryHelper
 import com.example.evgeny.setlist_mobile.utils.SetlistsRepository
 import dagger.Module
@@ -14,6 +17,17 @@ class DataModule(val context: Context) {
 
     @Provides
     fun provideContext(): Context = context
+
+    @Singleton
+    @Provides
+    fun provideArtistDao(): ArtistDao {
+        val database = Room.databaseBuilder(
+                context,
+                AppDataBase::class.java,
+                AppDataBase.SAVED_ARTISTS_TABLE_NAME
+        ).build().artistDao()
+        return database
+    }
 
     @Singleton
     @Provides
