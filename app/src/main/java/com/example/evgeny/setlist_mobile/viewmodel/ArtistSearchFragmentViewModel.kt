033df5445e9bc60929ff36c315ac2e39edb60281
evgenykochkov <crabbs100@gmail.com>
2022-06-07 20:41:38 +0300

    убрал неиспользуемые поля, методы, библиотеки

--- app/src/main/java/com/example/evgeny/setlist_mobile/viewmodel/ArtistSearchFragmentViewModel.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/viewmodel/ArtistSearchFragmentViewModel.kt
@@ -4,8 +4,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.evgeny.setlist_mobile.App
 import com.example.evgeny.setlist_mobile.data.Artist
-import com.example.evgeny.setlist_mobile.data.entity.ArtistDataDTO
+import com.example.evgeny.setlist_mobile.data.SearchQuery
 import com.example.evgeny.setlist_mobile.data.entity.SetlistsDataDTO
+import com.example.evgeny.setlist_mobile.domain.Interactor
 import com.example.evgeny.setlist_mobile.utils.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -16,12 +17,16 @@ class ArtistSearchFragmentViewModel: ViewModel() {
 
     val artistsLiveData = MutableLiveData<List<Artist>>()
     val selectedArtistLiveData = SingleLiveEvent<Artist>()
+    val searchQueryArtistLiveData = MutableLiveData<List<SearchQuery>>()
+    val toastEventLiveData = SingleLiveEvent<String>()
+    val ARTIST_SEARCH_ON_FAILURE = "ничего не найдено"
+    val ARTIST_SEARCH_FIELD_IS_EMPTY = "введите что-то в поле поиска"
+
+    @Inject
+    lateinit var interactor: Interactor
 
     @Inject
     lateinit var setlistsRepository: SetlistsRepository
-
-    @Inject
-    lateinit var searchHistoryHelper: SearchHistoryHelper
 
     @Inject
     lateinit var setlistsRetrofit: SetlistsRetrofitInterface
@@ -32,29 +37,17 @@ class ArtistSearchFragmentViewModel: ViewModel() {
 
     fun searchArtist(artistName: String) {
         if (artistName.isNotEmpty()) {
-            setlistsRetrofit.searchArtists(
-                    artistName = artistName,
-                    page = 1,
-                    sort = SetlistsAPIConstants.SORT_TYPE_NAME
-            ).enqueue(object: Callback<ArtistDataDTO> {
-                override fun onResponse(call: Call<ArtistDataDTO>, response: Response<ArtistDataDTO>) {
-                    val artistList = Converter.convertArtistDTOListToArtistList(response.body()?.artist)
-                    if (artistList.isNotEmpty()) {
-                        setlistsRepository.setLastSearchArtists(artistList)
-                        if (!isArtistInHistory(artistName)) { searchHistoryHelper.saveSearchQuery(artistName) }
-                        //getView()?.showArtistList(artistList)
-                        artistsLiveData.postValue(artistList)
-                    } else {
-                        //getView()?.showToast("артистов не найдено")
-                    }
+            interactor.searchArtist(artistName, object : Interactor.OnRetrofitCallback {
+                override fun onSuccess(list: List<Artist>) {
+                    artistsLiveData.postValue(list)
                 }
 
-                override fun onFailure(call: Call<ArtistDataDTO>, t: Throwable) {
-                    //getView()?.showToast("поиск не удался")
+                override fun onFailure() {
+                    toastEventLiveData.postValue(ARTIST_SEARCH_ON_FAILURE)
                 }
             })
         } else {
-            //getView()?.showToast("поле пустое")
+            toastEventLiveData.postValue(ARTIST_SEARCH_FIELD_IS_EMPTY)
         }
     }
 
@@ -83,20 +76,7 @@ class ArtistSearchFragmentViewModel: ViewModel() {
         })
     }
 
-    private fun isArtistInHistory(artistName: String): Boolean {
-        var isInHistory = false
-        val searchList = searchHistoryHelper.getHistorySearchList()
-        if (searchList.isNotEmpty()) {
-            var size = searchList.size
-            do {
-                size-=1
-                if (searchList[size] == artistName) { isInHistory = true }
-            } while (!isInHistory && size>0)
-        }
-        return isInHistory
-    }
-
-    fun getHistorySearchList(): List<String> {
-        return searchHistoryHelper.getHistorySearchList()
+    fun getSearchQueryArtists(): List<String> {
+        return setlistsRepository.getSearchQueryArtists()
     }
 }
