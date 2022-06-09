f6665c4eeb73b716cd85fc17ebbf90c563f02a65
evgenykochkov <crabbs100@gmail.com>
2022-06-09 20:06:15 +0300

    добавил в БД таблицу для истории поиска

--- app/src/main/java/com/example/evgeny/setlist_mobile/data/AppDataBase.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/data/AppDataBase.kt
@@ -4,12 +4,15 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.evgeny.setlist_mobile.data.dao.ArtistDao
 
-@Database(entities = [Artist::class], version = 1, exportSchema = false)
+@Database(entities = [Artist::class, SearchQuery::class], version = 1, exportSchema = false)
 abstract class AppDataBase: RoomDatabase() {
 
     abstract fun artistDao(): ArtistDao
 
     companion object {
         const val SAVED_ARTISTS_TABLE_NAME = "artists_table"
+        const val SAVED_SEARCH_QUERRY_TABLE_NAME = "saved_search_query_table"
+
+        const val SEARCH_TYPE_ARTISTS = 1
     }
 }
