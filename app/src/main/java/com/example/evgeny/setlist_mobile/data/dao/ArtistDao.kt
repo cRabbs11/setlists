573cd62def825fd1a011cb8b5b4b804dc1c4799a
evgenykochkov <crabbs100@gmail.com>
2022-06-09 20:06:39 +0300

    к предыдущему коммиту

--- app/src/main/java/com/example/evgeny/setlist_mobile/data/dao/ArtistDao.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/data/dao/ArtistDao.kt
@@ -3,6 +3,7 @@ package com.example.evgeny.setlist_mobile.data.dao
 import androidx.room.*
 import com.example.evgeny.setlist_mobile.data.AppDataBase
 import com.example.evgeny.setlist_mobile.data.Artist
+import com.example.evgeny.setlist_mobile.data.SearchQuery
 
 
 @Dao
@@ -22,4 +23,10 @@ interface ArtistDao {
 
     @Update
     fun updateArtist(artist: Artist)
+
+    @Query("SELECT * FROM ${AppDataBase.SAVED_SEARCH_QUERRY_TABLE_NAME}")
+    fun getSearchQueryArtists() : List<SearchQuery>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertSearchQuery(searchQuery: SearchQuery)
 }
