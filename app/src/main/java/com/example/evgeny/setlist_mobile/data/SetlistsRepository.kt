598d45fa9de0c6591aaa4d4d3a074fe5bae2269e
evgenykochkov <crabbs100@gmail.com>
2022-06-15 20:24:41 +0300

    сделал методы проверки наличия сетлистов и получения сетлистов черезе Observable. Добавил методы для добавления/удаления сетлистов в БД

--- app/src/main/java/com/example/evgeny/setlist_mobile/data/SetlistsRepository.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/data/SetlistsRepository.kt
@@ -3,35 +3,32 @@ package com.example.evgeny.setlist_mobile.data
 import com.example.evgeny.setlist_mobile.data.dao.ArtistDao
 import com.example.evgeny.setlist_mobile.data.entity.Setlist
 import com.example.evgeny.setlist_mobile.setlists.SongListItem
-import io.reactivex.rxjava3.subjects.BehaviorSubject
+import com.example.evgeny.setlist_mobile.utils.Converter
+import com.example.evgeny.setlist_mobile.utils.SetlistsRetrofitInterface
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.Executors
 
-class SetlistsRepository(val artistDao: ArtistDao) {
+class SetlistsRepository(private val artistDao: ArtistDao, private val retrofit: SetlistsRetrofitInterface) {
 
     val TAG = SetlistsRepository::class.java.name + " BMTH "
 
     private val lastSearchArtists = ArrayList<Artist>()
-    private var setlists = ArrayList<Setlist>()
     private lateinit var selectedArtist : Artist
     private lateinit var currentSetlist: Setlist
     private var songlist =  ArrayList<SongListItem>()
     private var setlistPage = 1
 
-    val setlistSubject = BehaviorSubject.create<List<Setlist>>()
-
     fun setSelectedArtist(artist: Artist) {
-        selectedArtist = artist
-        this.setlists.clear()
-        setlistPage = 1
+        Executors.newSingleThreadExecutor().execute {
+            selectedArtist = artist
+            clearSetlistsInDB()
+            setlistPage = 1
+        }
     }
 
     fun getSelectedArtist() :  Artist? {
         return selectedArtist
-    }
-
-    fun addToSetlists(setlists: List<Setlist>) {
-        this.setlists.addAll(setlists)
-        increaseSetlistPage()
-        setlistSubject.onNext(this.setlists)
     }
 
     fun setLastSearchArtists(list: List<Artist>) {
@@ -39,10 +36,6 @@ class SetlistsRepository(val artistDao: ArtistDao) {
         list.forEach {
             lastSearchArtists.add(it)
         }
-    }
-
-    fun getSetlists(): ArrayList<Setlist> {
-        return setlists
     }
 
     fun setCurrentSetlist(setlist: Setlist) {
@@ -63,7 +56,7 @@ class SetlistsRepository(val artistDao: ArtistDao) {
 
     fun setSongList(setlist: Setlist) {
         val newSongList = arrayListOf<SongListItem>()
-        setlist.sets.forEach {
+        setlist.sets!!.forEach {
             if (it.name!="") {
                 newSongList.add(it)
             } else if (it.encore>0) {
@@ -91,5 +84,56 @@ class SetlistsRepository(val artistDao: ArtistDao) {
 
     fun saveSearchQueryArtists(query: SearchQuery) {
         return artistDao.insertSearchQuery(query)
+    }
+
+    private fun insertSetlistsInDB(list: List<Setlist>) {
+        artistDao.insertSetlists(list)
+        println("insert done1")
+    }
+
+    private fun clearSetlistsInDB() {
+        artistDao.clearSetlists()
+    }
+
+    fun isSetlistsHave(artist: Artist): Observable<Boolean> {
+        return retrofit.getSetlistsByArtistObservable(
+            artistMbid = artist.mbid,
+            page = 1
+        ).subscribeOn(Schedulers.io())
+            .onErrorComplete{
+                false
+            }
+            .flatMap {
+                val list = Converter.convertSetlistDTOListToSetlistList(it.setlist)
+                if (list.isNotEmpty()) {
+                    Observable.just(true)
+                } else {
+                    Observable.just(false)
+                }
+            }
+    }
+
+    fun getSetlists(artist: Artist): Observable<List<Setlist>> {
+        return Observable.concat(
+            retrofit.getSetlistsByArtistObservable(
+                artistMbid = artist.mbid,
+                page = getSetlistPage()
+            ).subscribeOn(Schedulers.io())
+                .onErrorComplete{
+                    false
+                }
+                .map {
+                    Converter.convertSetlistDTOListToSetlistList(it.setlist)
+                }
+                .flatMap {
+                    println("page = ${getSetlistPage()}, list.size = ${it.size}")
+                    if (it.isNotEmpty()) {
+                        insertSetlistsInDB(it)
+                        increaseSetlistPage()
+                    }
+                    Observable.empty<List<Setlist>>()
+                },
+            artistDao.getSetlists().subscribeOn(Schedulers.io())
+        )
     }
 }
