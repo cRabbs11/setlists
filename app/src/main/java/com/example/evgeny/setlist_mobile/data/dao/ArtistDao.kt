97680419cd74823db8ecf3857f536d99c895b26a
evgenykochkov <crabbs100@gmail.com>
2022-06-15 20:21:23 +0300

    добавил в Dao работу с таблицей сетлистов

--- app/src/main/java/com/example/evgeny/setlist_mobile/data/dao/ArtistDao.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/data/dao/ArtistDao.kt
@@ -4,6 +4,8 @@ import androidx.room.*
 import com.example.evgeny.setlist_mobile.data.AppDataBase
 import com.example.evgeny.setlist_mobile.data.Artist
 import com.example.evgeny.setlist_mobile.data.SearchQuery
+import com.example.evgeny.setlist_mobile.data.entity.Setlist
+import io.reactivex.rxjava3.core.Observable
 
 
 @Dao
@@ -21,6 +23,9 @@ interface ArtistDao {
     @Delete
     fun deleteArtist(artist: Artist)
 
+    @Query("DELETE FROM ${AppDataBase.CASHED_SETLISTS_TABLE_NAME}")
+    fun clearSetlists()
+
     @Update
     fun updateArtist(artist: Artist)
 
@@ -29,4 +34,10 @@ interface ArtistDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertSearchQuery(searchQuery: SearchQuery)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertSetlists(setlists: List<Setlist>)
+
+    @Query("SELECT * FROM ${AppDataBase.CASHED_SETLISTS_TABLE_NAME}")
+    fun getSetlists(): Observable<List<Setlist>>
 }
