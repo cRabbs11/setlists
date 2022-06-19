5b4f47aa7947ee54cf5df36a02e0b56b3a6cfeea
evgenykochkov <crabbs100@gmail.com>
2022-06-19 12:55:55 +0300

    убрал ниеспользуемый inject

--- app/src/main/java/com/example/evgeny/setlist_mobile/viewmodel/SingleSetlistFragmentViewModel.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/viewmodel/SingleSetlistFragmentViewModel.kt
@@ -5,17 +5,12 @@ import androidx.lifecycle.ViewModel
 import com.example.evgeny.setlist_mobile.App
 import com.example.evgeny.setlist_mobile.data.entity.Setlist
 import com.example.evgeny.setlist_mobile.setlists.SongListItem
-import com.example.evgeny.setlist_mobile.data.SetlistsRepository
 import com.example.evgeny.setlist_mobile.utils.SetlistHelper
-import javax.inject.Inject
 
 class SingleSetlistFragmentViewModel(private val setlist: Setlist): ViewModel() {
 
     var songListItemLiveData = MutableLiveData<List<SongListItem>>()
     var setlistInfoLiveData = MutableLiveData<Setlist>()
-
-    @Inject
-    lateinit var setlistsRepository: SetlistsRepository
 
     init {
         App.instance.dagger.inject(this)
