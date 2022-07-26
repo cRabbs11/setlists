d4d61c4eecbfab7ffc9e0545fd8a60bfd4163e1e
evgenykochkov <crabbs100@gmail.com>
2022-07-26 10:13:15 +0300

    create MapFragment with MVVM

--- app/src/main/java/com/example/evgeny/setlist_mobile/viewmodel/MapFragmentViewModel.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/viewmodel/MapFragmentViewModel.kt
@@ -1,6 +1,14 @@
 package com.example.evgeny.setlist_mobile.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.evgeny.setlist_mobile.data.entity.Venue
 
-class MapFragmentViewModel() : ViewModel() {
+class MapFragmentViewModel(private val venue: Venue) : ViewModel() {
+
+    val venueLiveData = MutableLiveData<Venue>()
+
+    init {
+        venueLiveData.postValue(venue)
+    }
 }
