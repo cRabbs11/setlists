aaaa76b40b21b0152c7c354a047525ccdd39abb2
evgenykochkov <crabbs100@gmail.com>
2022-07-26 10:10:53 +0300

    add constants

--- app/src/main/java/com/example/evgeny/setlist_mobile/utils/Constants.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/utils/Constants.kt
@@ -5,9 +5,11 @@ object Constants {
     const val ARTIST_SEARCH_FIELD_IS_EMPTY = "введите что-то в поле поиска"
     const val SETLISTS_SEARCH_FAILURE = "поиск сетлистов неудался"
     const val SETLISTS_SEARCH_NOT_FOUND = "сетлистов не найдено"
+    const val VENUE_DATA_NOT_FOUND = "данные не найдены"
 
     const val KEY_BUNDLE_ARTIST = "artist"
     const val KEY_BUNDLE_SETLIST = "setlist"
+    const val KEY_BUNDLE_VENUE = "venue"
     const val KEY_BUNDLE_TRANSITION = "transition"
 
     const val EXCEPTION_MESSAGE_ARGUMENT_IS_NULL = "Required argument is null"
