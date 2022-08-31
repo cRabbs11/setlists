90d237dee4d12413e3bf8a148eccbf8987977239
evgenykochkov <crabbs100@gmail.com>
2022-08-31 20:17:22 +0300

    добавил Timber

--- app/src/main/java/com/example/evgeny/setlist_mobile/App.kt
+++ app/src/main/java/com/example/evgeny/setlist_mobile/App.kt
@@ -6,12 +6,18 @@ import com.example.evgeny.setlist_mobile.di.DaggerAppComponent
 import com.example.evgeny.setlist_mobile.di.modules.DataModule
 import com.example.evgeny.setlist_mobile.di.modules.DomainModule
 import com.example.evgeny.setlist_mobile.di.modules.RemoteModule
+import timber.log.Timber
+import timber.log.Timber.Forest.plant
 
 class App: Application() {
     lateinit var dagger: AppComponent
 
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            plant(Timber.DebugTree())
+        }
         instance = this
         dagger = DaggerAppComponent.builder()
             .dataModule(DataModule(this))
