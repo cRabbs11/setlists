6377098665a0fe5cf8ac31d04444d02d7864fd8a
evgenykochkov <crabbs100@gmail.com>
2024-03-08 13:13:31 +0300

    применил кастомную тему

--- app/src/main/java/com/kochkov/evgeny/setlist_mobile/view/fragments/ArtistsFragment.kt
+++ app/src/main/java/com/kochkov/evgeny/setlist_mobile/view/fragments/ArtistsFragment.kt
@@ -1,17 +1,23 @@
 package com.kochkov.evgeny.setlist_mobile.view.fragments
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.kochkov.evgeny.setlist_mobile.data.Artist
 import com.kochkov.evgeny.setlist_mobile.utils.OnItemClickListener
 import com.kochkov.evgeny.setlist_mobile.view.activities.MainActivity
-import com.kochkov.evgeny.setlist_mobile.ui.SearchCompose
+import com.kochkov.evgeny.setlist_mobile.ui.MyTheme
+import com.kochkov.evgeny.setlist_mobile.ui.SearchArtistCompose
 import com.kochkov.evgeny.setlist_mobile.viewmodel.ArtistSearchFragmentViewModel
 
 class ArtistsFragment: Fragment() {
@@ -26,17 +32,35 @@ class ArtistsFragment: Fragment() {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
-                val search = SearchCompose()
-                search.SearchArtistCompose(
-                    clickListener = object : OnItemClickListener<Artist> {
-                        override fun onItemClick(artist: Artist) {
-                            (activity as MainActivity).openSetlistsSearchFragment(artist)
-                        }
-                    },
-                    viewModel = viewModel
-                )
+                MyTheme {
+                    SearchArtistCompose(
+                        clickListener = object : OnItemClickListener<Artist> {
+                            override fun onItemClick(artist: Artist) {
+                                (activity as MainActivity).openSetlistsSearchFragment(artist)
+                            }
+                        },
+                        viewModel = viewModel
+                    )
+                }
             }
 
+        }
+    }
+}
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    name = "DefaultPreviewLight"
+)
+@Composable
+fun ReplyAppPreview() {
+    MyTheme {
+        Button(onClick = {}) {
+            Text("some text")
         }
     }
 }
