af67fbcf29a96b942400fc0836e4943acd74c983
cRabbs11 <cRabbs100@gmail.com>
2024-09-20 00:57:35 +0300

    добавил в Setlist метод для получения списка SongListItem

--- app/src/main/java/com/kochkov/evgeny/setlist_mobile/data/entity/Setlist.kt
+++ app/src/main/java/com/kochkov/evgeny/setlist_mobile/data/entity/Setlist.kt
@@ -4,6 +4,7 @@ import androidx.room.*
 import com.kochkov.evgeny.setlist_mobile.data.AppDataBase
 import com.kochkov.evgeny.setlist_mobile.data.Artist
 import com.kochkov.evgeny.setlist_mobile.setlists.BaseModel
+import com.kochkov.evgeny.setlist_mobile.setlists.SongListItem
 
 @Entity(tableName = AppDataBase.CASHED_SETLISTS_TABLE_NAME)
 data class Setlist(
@@ -13,12 +14,21 @@ data class Setlist(
     @Embedded(prefix = "tour_") var tour: Tour?,
     @ColumnInfo(name = "eventDate") var eventDate: String,
     @ColumnInfo(name = "lastUpdated") var lastUpdated: String,
-    var sets: List<Set>?) : BaseModel() {
-        constructor(): this(
-            artist = null,
-            venue = null,
-            tour = null,
-            eventDate = "null",
-            lastUpdated = "null",
-            sets = null)
+    var sets: List<Set> = listOf()
+) : BaseModel() {
+
+    fun getComponentsList(): List<SongListItem> {
+        val newSongList = arrayListOf<SongListItem>()
+        sets.forEach {set ->
+            if (set.name!="") {
+                newSongList.add(set)
+            } else if (set.encore>0) {
+                newSongList.add(set)
+            }
+            set.songs.forEach { song ->
+                newSongList.add(song)
+            }
+        }
+        return newSongList
+    }
 }
