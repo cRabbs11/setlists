38c6c5a6638dbff72a3d86b27ceb191c95d10435
cRabbs11 <cRabbs100@gmail.com>
2024-09-20 00:31:46 +0300

    добавил в SetlistHelper методы для перевода SetlistsDataDTO в список Setlists и из SetlistDTO в Setlist

--- app/src/main/java/com/kochkov/evgeny/setlist_mobile/utils/SetlistHelper.kt
+++ app/src/main/java/com/kochkov/evgeny/setlist_mobile/utils/SetlistHelper.kt
@@ -1,5 +1,12 @@
 package com.kochkov.evgeny.setlist_mobile.utils
 
+import com.kochkov.evgeny.setlist_mobile.data.dto.SetlistDTO
+import com.kochkov.evgeny.setlist_mobile.data.dto.SetlistsDataDTO
+import com.kochkov.evgeny.setlist_mobile.data.dto.toArtist
+import com.kochkov.evgeny.setlist_mobile.data.dto.toSet
+import com.kochkov.evgeny.setlist_mobile.data.dto.toTour
+import com.kochkov.evgeny.setlist_mobile.data.dto.toVenue
+import com.kochkov.evgeny.setlist_mobile.data.entity.Set
 import com.kochkov.evgeny.setlist_mobile.data.entity.Setlist
 import com.kochkov.evgeny.setlist_mobile.setlists.SongListItem
 
@@ -19,4 +26,30 @@ object SetlistHelper {
         }
         return newSongList
     }
+
+    fun fromSetlistDataDTOtoSetlists(setlistsDataDTO: SetlistsDataDTO): List<Setlist> {
+        val list = arrayListOf<Setlist>()
+        setlistsDataDTO.setlist.forEach {setlistDTO ->
+            list.add(fromSetlistDTOtoSetlist(setlistDTO))
+        }
+        return list
+    }
+
+    fun fromSetlistDTOtoSetlist(setlistDTO: SetlistDTO): Setlist {
+        val list = arrayListOf<Set>()
+        setlistDTO.sets.set.forEach {
+            list.add(it.toSet())
+        }
+        return Setlist(
+            id = setlistDTO.id.hashCode(),
+            artist = setlistDTO.artist.toArtist(),
+            venue = setlistDTO.venue.toVenue(),
+            tour = setlistDTO.tour?.toTour(),
+            eventDate = setlistDTO.eventDate,
+            lastUpdated = setlistDTO.lastUpdated,
+            sets = list
+        )
+    }
+
+
 }
