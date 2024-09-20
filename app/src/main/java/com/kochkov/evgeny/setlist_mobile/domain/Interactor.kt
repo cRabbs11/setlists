063496cb9620f2a17f9372e45e9acce3c8134f3e
cRabbs11 <cRabbs100@gmail.com>
2024-09-20 14:21:54 +0300

    перенес логику получения артистов с сетлистами в интерактор. поправил имя repository в интеракторе

--- app/src/main/java/com/kochkov/evgeny/setlist_mobile/domain/Interactor.kt
+++ app/src/main/java/com/kochkov/evgeny/setlist_mobile/domain/Interactor.kt
@@ -20,13 +20,70 @@ class Interactor(private val remoteRepository: RemoteRepository, private val loc
         }
     }
 
-    suspend fun searchArtistWithSetlists(artistName: String) = repository.searchArtistWithSetlists(artistName)
+    suspend fun getSearchQueryArtists() = localRepository.getSearchQueryArtists()
 
-    suspend fun isHaveSetlists(artist: Artist) = repository.isSetlistsHave(artist)
+    suspend fun searchArtistsWithSetlists(artistName: String): List<Artist> {
+        return coroutineScope {
+            val list = remoteRepository.searchArtists(artistName)
+            val artistsWithSetlists = arrayListOf<Artist>()
+            val deferred = list.flatMap {
+                listOf(
+                    async {
+                        if (isArtistHaveSetlists(it)) {
+                            it
+                        } else {null}
+                    }
+                )
+            }
 
-    suspend fun getSetlists(artist: Artist, page: Int) = repository.getSetlists(artist, page)
+            deferred.awaitAll().forEach { artist ->
+                artist?.let {
+                    artistsWithSetlists.add(it)
+                }
+            }
+            if (artistsWithSetlists.isNotEmpty()) {
+                val searchQuery = SearchQuery(queryText = artistName, searchType = AppDataBase.SEARCH_TYPE_ARTISTS)
+                localRepository.saveSearchQuery(searchQuery)
+            }
+            artistsWithSetlists
+        }
+    }
 
-    suspend fun getSetlistsInTour(tourName: String) = repository.getSetlistsInTour(tourName)
+    suspend fun isHaveSetlists(artist: Artist) = remoteRepository.isSetlistsHave(artist)
 
-    fun setlistPagingSource(artist: String) = repository.setlistPagingSource(artist)
+    suspend fun getSetlists(artist: Artist, page: Int) = remoteRepository.getSetlists(artist, page)
+
+    suspend fun getSetlistsInTour(tourName: String) = remoteRepository.getAllSetlistsInTour(tourName)
+
+    fun setlistPagingSource(artist: String) = remoteRepository.setlistPagingSource(artist)
+
+    private suspend fun isArtistHaveSetlists(artist: Artist): Boolean {
+        return coroutineScope {
+            remoteRepository.getSetlists(artist, 1).isNotEmpty()
+        }
+    }
+
+    //код на rxJava для получения сетлистов и с БД и с сети
+    //fun getSetlistsWithDB(artist: Artist, page: Int): Observable<List<Setlist>> {
+    //    return Observable.concat(
+    //        retrofit.getSetlistsByArtistObservable(
+    //            artistMbid = artist.mbid,
+    //            page = page
+    //        ).subscribeOn(Schedulers.io())
+    //            .onErrorComplete{
+    //                false
+    //            }
+    //            .map {
+    //                val list = it.toSetlistList()
+    //                list
+    //            }
+    //            .flatMap {
+    //                if (it.isNotEmpty()) {
+    //                    insertSetlistsInDB(it)
+    //                }
+    //                Observable.empty<List<Setlist>>()
+    //            },
+    //        artistDao.getSetlists().subscribeOn(Schedulers.io())
+    //    )
+    //}
 }
