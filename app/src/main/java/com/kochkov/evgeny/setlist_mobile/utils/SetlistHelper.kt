5b36de2c7107dfd236b18a85f6ef91fc1fb91c02
cRabbs11 <cRabbs100@gmail.com>
2024-09-20 14:13:34 +0300

    добавил методы для конвертации DTO объектов в SetlistHelper

--- app/src/main/java/com/kochkov/evgeny/setlist_mobile/utils/SetlistHelper.kt
+++ app/src/main/java/com/kochkov/evgeny/setlist_mobile/utils/SetlistHelper.kt
@@ -1,20 +1,25 @@
 package com.kochkov.evgeny.setlist_mobile.utils
 
+import com.kochkov.evgeny.setlist_mobile.data.Artist
+import com.kochkov.evgeny.setlist_mobile.data.dto.ArtistDTO
+import com.kochkov.evgeny.setlist_mobile.data.dto.ArtistDataDTO
+import com.kochkov.evgeny.setlist_mobile.data.dto.SetDTO
 import com.kochkov.evgeny.setlist_mobile.data.dto.SetlistDTO
 import com.kochkov.evgeny.setlist_mobile.data.dto.SetlistsDataDTO
 import com.kochkov.evgeny.setlist_mobile.data.dto.toArtist
-import com.kochkov.evgeny.setlist_mobile.data.dto.toSet
+import com.kochkov.evgeny.setlist_mobile.data.dto.toSong
 import com.kochkov.evgeny.setlist_mobile.data.dto.toTour
 import com.kochkov.evgeny.setlist_mobile.data.dto.toVenue
 import com.kochkov.evgeny.setlist_mobile.data.entity.Set
 import com.kochkov.evgeny.setlist_mobile.data.entity.Setlist
+import com.kochkov.evgeny.setlist_mobile.data.entity.Song
 import com.kochkov.evgeny.setlist_mobile.setlists.SongListItem
 
 object SetlistHelper {
 
     fun fromSetlistToSongList(setlist: Setlist): List<SongListItem> {
         val newSongList = arrayListOf<SongListItem>()
-        setlist.sets!!.forEach { set ->
+        setlist.sets.forEach { set ->
             if (set.name!="") {
                 newSongList.add(set)
             } else if (set.encore>0) {
@@ -27,6 +32,24 @@ object SetlistHelper {
         return newSongList
     }
 
+    fun fromArtistDataDTOToArtists(artistDataDTO: ArtistDataDTO): List<Artist> {
+        val list = arrayListOf<Artist>()
+        artistDataDTO.artist.forEach { artistDTO ->
+            list.add(fromArtistDTOToArtist(artistDTO))
+        }
+        return list
+    }
+
+    fun fromArtistDTOToArtist(artistDTO: ArtistDTO): Artist {
+        return Artist(
+            disambiguation = artistDTO.disambiguation ?: "",
+            mbid = artistDTO.mbid,
+            name = artistDTO.name,
+            sortName = artistDTO.sortName,
+            tmid = artistDTO.tmid,
+            url = artistDTO.url)
+    }
+
     fun fromSetlistDataDTOtoSetlists(setlistsDataDTO: SetlistsDataDTO): List<Setlist> {
         val list = arrayListOf<Setlist>()
         setlistsDataDTO.setlist.forEach {setlistDTO ->
@@ -35,10 +58,10 @@ object SetlistHelper {
         return list
     }
 
-    fun fromSetlistDTOtoSetlist(setlistDTO: SetlistDTO): Setlist {
+    private fun fromSetlistDTOtoSetlist(setlistDTO: SetlistDTO): Setlist {
         val list = arrayListOf<Set>()
         setlistDTO.sets.set.forEach {
-            list.add(it.toSet())
+            list.add(fromSetDTOToSet(it))
         }
         return Setlist(
             id = setlistDTO.id.hashCode(),
@@ -51,5 +74,20 @@ object SetlistHelper {
         )
     }
 
-
+    private fun fromSetDTOToSet(setDTO: SetDTO): Set {
+        var songNumber = 1
+        val list = arrayListOf<Song>()
+        setDTO.song.forEach {
+            val number = if (!it.tape) {
+                songNumber++
+            } else {
+                songNumber
+            }
+            list.add(it.toSong(number))
+        }
+        return Set(
+            name = setDTO.name?: "",
+            encore = setDTO.encore,
+            songs = list)
+    }
 }
