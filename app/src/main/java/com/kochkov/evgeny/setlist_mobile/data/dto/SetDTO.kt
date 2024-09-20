9848f3e88d0dd7fc609f5f51126c8f2805bb809e
cRabbs11 <cRabbs100@gmail.com>
2024-09-20 14:15:09 +0300

    убрал лишнюю функцию

--- app/src/main/java/com/kochkov/evgeny/setlist_mobile/data/dto/SetDTO.kt
+++ app/src/main/java/com/kochkov/evgeny/setlist_mobile/data/dto/SetDTO.kt
@@ -1,8 +1,6 @@
 package com.kochkov.evgeny.setlist_mobile.data.dto
 
 import com.google.gson.annotations.SerializedName
-import com.kochkov.evgeny.setlist_mobile.data.entity.Set
-import com.kochkov.evgeny.setlist_mobile.data.entity.Song
 
 data class SetDTO(
         @SerializedName("encore")
@@ -12,20 +10,3 @@ data class SetDTO(
         @SerializedName("song")
         val song: List<SongDTO>
 )
-
-fun SetDTO.toSet(): Set {
-    var songNumber = 1
-    val list = arrayListOf<Song>()
-    song.forEach {
-        val number = if (!it.tape) {
-            songNumber++
-        } else {
-            songNumber
-        }
-        list.add(it.toSong(number))
-    }
-    return Set(
-        name = name?: "",
-        encore = encore,
-        songs = list)
-}
