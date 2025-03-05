0505b0c88177a3a46982f3969a9b084a2bd19658
cRabbs11 <cRabbs100@gmail.com>
2025-03-05 21:38:21 +0300

    Test1: add testClass

---
+++ app/src/main/java/com/kochkov/evgeny/setlist_mobile/remote/TestingClass.kt
@@ -0,0 +1,12 @@
+package com.kochkov.evgeny.setlist_mobile.remote
+
+class TestingClass {
+
+    private var some = "some"
+
+    fun getSome() = some
+
+    fun setSome(some: String) {
+        this.some = some
+    }
+}
